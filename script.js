e8dc8e68dc85a387db9b69b763706b64a56b0453
Felipe Dev <felipe_souza22@live.com>
2025-08-28 12:48:59 -0300

    ja esta finalizado

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function validateNewProduct() {
 function newProduct() {
 
     let input = document.getElementById('input_new_product');
-
+    input.style.border = ''
 
     if (!input.value) {
+        input.style.border = '2px solid red'
         alert('Digite o nome do produto!!')
     }
     else if(validateNewProduct()){
@@ -31,8 +32,7 @@ function newProduct() {
         localStorage.setItem(localStorageKey,JSON.stringify(values))
         showValues()
     }
-
-    input.values = ''
+    input.value = ''
 }
 window.newProduct = newProduct;
 
@@ -42,7 +42,7 @@ function showValues() {
     let list = document.getElementById('to_do_list')
     list.innerHTML =''
     for(let i = 0; i < values.length; i++){
-        list.innerHTML += `<li>${values [i].name}<button id='btn' onclick='removeItem("${values [i].name}")' >OK</button></li>`
+        list.innerHTML += `<li>${values [i].name}<button id='btn' onclick='removeItem("${values [i].name}")' ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg></button></li>`
     }
     
 }
@@ -55,5 +55,8 @@ function removeItem(data) {
     localStorage.setItem(localStorageKey,JSON.stringify(values))
     showValues();
 }
+
+window.removeItem = removeItem;
+
 
 showValues();
